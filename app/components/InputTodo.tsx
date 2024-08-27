6db5eb1939ceb607d6f81be3a93727f3566012b7
Neoncodeing <neonoile19@gmail.com>
2024-08-27 18:24:07 +0900

    Update

--- app/components/InputTodo.tsx
+++ app/components/InputTodo.tsx
@@ -4,77 +4,88 @@ import React, { useState, useEffect } from "react";
 import { TypeTodo } from "../types/types";
 import TodoList from "./TodoList";
 
+// TODO入力エリアのコンポーネント
 const InputTodo = () => {
-	const [todo, setTodo] = useState<string>("");
-	const [todos, setTodos] = useState<TypeTodo[]>([]);
-	const [isInitialLoad, setIsInitialLoad] = useState<boolean>(true);
+    const [todo, setTodo] = useState<string>("");
+    const [todos, setTodos] = useState<TypeTodo[]>([]);
+    const [isInitialLoad, setIsInitialLoad] = useState<boolean>(true);
 
-	useEffect(() => {
-		const savedTodos = localStorage.getItem("todos");
-		setTodos(savedTodos ? JSON.parse(savedTodos) : []);
+    // ローカルストレージに保存されたTODOを読み込み
+    useEffect(() => {
+        const savedTodos = localStorage.getItem("todos");
+        setTodos(savedTodos ? JSON.parse(savedTodos) : []);
 
-		setIsInitialLoad(false);
-	}, []);
+        setIsInitialLoad(false);
+    }, []);
 
-	const handleChange = (
-		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-	) => {
-		setTodo(e.target.value);
-	};
+    // 入力されたTODOの状態を管理する関数
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        setTodo(e.target.value);
+    };
 
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		const submitter = (e.nativeEvent as SubmitEvent)
-			.submitter as HTMLButtonElement;
+    // フォーム送信時にTODOを作成する関数
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-		if (submitter.value === "delete") {
-			console.log(submitter.value);
-			localStorage.removeItem("todos");
-			setTodos([]);
-		} else {
-			if (todo) {
-				const newTodo: TypeTodo = {
-					id: todos.length + 1,
-					title: todo,
-				};
-				setTodos([...todos, newTodo]);
-			}
-		}
-		setTodo("");
-	};
+        // どのボタンが押されたかを取得
+        const submitter = (e.nativeEvent as SubmitEvent)
+            .submitter as HTMLButtonElement;
 
-	useEffect(() => {
-		if (!isInitialLoad) {
-			localStorage.setItem("todos", JSON.stringify(todos));
-		}
-	}, [todos, isInitialLoad]);
+		// 押されたボタンによって処理を振り分け
+        if (submitter.value === "delete") {
+            setTodos([]);
+            setTodo("");
+            localStorage.removeItem("todos");
+        } else {
+            if (todo) {
+                const newTodo: TypeTodo = {
+                    id: todos.length + 1,
+                    title: todo,
+                    completed: false,
+                };
+                setTodos([...todos, newTodo]);
+            }
+            setTodo("");
+        }
+    };
 
-	return (
-		<>
-			<div className="container">
-				<form className="" onSubmit={handleSubmit}>
-					<input
-						className="input"
-						type="text"
-						name="title"
-						value={todo}
-						onChange={handleChange}
-					/>
+	// 初回ロード時はローカルストレージを更新しない
+    useEffect(() => {
+        if (!isInitialLoad) {
+            localStorage.setItem("todos", JSON.stringify(todos));
+        }
+    }, [todos, isInitialLoad]);
 
-					<div className="padding_container">
-						<button className="btn" value="create">
-							作成
-						</button>
-						<button className="btn" value="delete">
-							リセット
-						</button>
-					</div>
-				</form>
-				<TodoList todos={todos} setTodos={setTodos} />
-			</div>
-			<div className="spacer"></div>
-		</>
-	);
+	// コンポーネント出力部
+    return (
+        <>
+            <div className="container">
+                <form className="" onSubmit={handleSubmit}>
+                    <input
+                        className="input"
+                        type="text"
+                        name="title"
+                        value={todo}
+                        onChange={handleChange}
+                    />
+
+                    <div className="padding_container">
+                        <button className="btn" value="create">
+                            作成
+                        </button>
+                        <button className="btn" value="delete">
+                            リセット
+                        </button>
+                    </div>
+                </form>
+				{/* TODOリストをコンポーネントから呼び出し */}
+                <TodoList todos={todos} setTodos={setTodos} />
+            </div>
+            <div className="spacer"></div>
+        </>
+    );
 };
 
 export default InputTodo;
--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,47 +1,86 @@
 "use client";
 import { TypeTodo } from "../types/types";
-import { FaClipboard } from "react-icons/fa";
-import { FaTrashAlt } from "react-icons/fa";
+import { FaTrashAlt, FaClipboard, FaClipboardCheck } from "react-icons/fa";
+import React, { useState } from "react";
 
+// 受け取るPropsの型定義
 type Props = {
 	todos: TypeTodo[];
 	setTodos: React.Dispatch<React.SetStateAction<TypeTodo[]>>;
 };
 
+// 入力されたTODOをリスト表示するコンポーネント
 const TodoList = ({ todos, setTodos }: Props) => {
-	const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+	const [copiedTodoId, setCopiedTodoId] = useState<number | null | undefined>(null);
+
+	// TODOの完了状態を管理する関数
+	const handleChecked = (e: React.ChangeEvent<HTMLInputElement>)=>{
+		const targetId = Number(e.currentTarget.value);
+		const newTodos = todos.map((todo)=>{
+			if(todo.id === targetId){
+				return {
+					...todo,
+					completed:!todo.completed
+				}
+			}
+			return todo;
+		})
+		setTodos([...newTodos])
+	}
+
+	// TODOをクリップボードにコピーする関数
+	const handleCopy = (id: number | null, title: string | undefined) => {
+
+		// コピーが押された状態を管理しsetTimeoutでIDがはずれるまでの間出力部でアイコンを変更
+		navigator.clipboard
+			.writeText(title || "")
+			.then(() => {
+				setCopiedTodoId(id);
+
+				setTimeout(() => {
+					setCopiedTodoId(null);
+				}, 1500);
+			})
+			.catch((err) => {
+				alert("todoをコピーできませんでした。");
+			});
+	};
+
+	// TODOを削除する関数
+	const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
 		const targetId = Number(e.currentTarget.value);
 		const newTodos = todos.filter((todo) => todo.id !== targetId);
 		setTodos([...newTodos]);
 	};
 
+	
+	// TODOリストのコンポーネント出力部
 	return (
 		<ul className="todo_list">
-			{/* {todos.map((todo) => (
-				<li key={todo.id}>
-					{todo.title}
-					<button
-						className="btn"
-						onClick={handleClick}
-						value={todo.id}
-					>
-						削除
-					</button>
-				</li>
-			))} */}
 			{todos.map((todo) => (
 				<li key={todo.id}>
-					{todo.title ? (todo.title.length > 25 ? todo.title.substring(0, 25) + '...' : todo.title.substring(0, 25)) : ''}
+					<input type="checkbox" checked={todo.completed} value={todo.id} onChange={handleChecked}/>
+					<span className={`todo_title ${todo.completed ? 'completed' : ''}`}>{todo.title}</span>
 					<div className="btn_wrap">
-						<button className="icon_btn" onClick={()=>{
-							navigator.clipboard.writeText(todo.title ? todo.title : '')
-							.then(()=>{
-								alert('todoをコピーしました。');
-							}).catch(err =>{
-								alert('todoをコピーできませんでした。')
-							});
-						}}><FaClipboard /></button>
-						<button className="icon_btn" onClick={handleClick} value={todo.id}>
+						<button
+							className="icon_btn"
+							data-unit="コピーする"
+							onClick={() =>
+								handleCopy(todo.id ? todo.id : null, todo.title)
+							}
+						>
+							{copiedTodoId === todo.id ? (
+								<FaClipboardCheck color="Green" />
+							) : (
+								<FaClipboard />
+							)}
+						</button>
+						<button
+							className="icon_btn"
+							data-unit="削除する"
+							onClick={handleDelete}
+							value={todo.id}
+						>
 							<FaTrashAlt />
 						</button>
 					</div>
